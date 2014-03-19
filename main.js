fb760b08af4e94a0014bdf2775b530842d37e9be
Derek Nassler <dnassler@rogers.com>
2014-03-19 11:38:26 -0400

    added icons for iphone and modified the game init to adapt the number of pixels based on the device but keeping the scale of the objects in the game

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 var pixelRatio = window.devicePixelRatio;
 var w = window.innerWidth ;//* pixelRatio,
     h = window.innerHeight ;//* pixelRatio;
+var lw, lh; //landscape width/height in pixels
+if ( h > w ) {
+	lw = h;
+	lh = w;
+} else {
+	lw = w;
+	lh = h;
+}
+var aspectRatioDevice = lw/lh;
 
 // const SAFE_ZONE_WIDTH=2048;
 // const SAFE_ZONE_HEIGHT=1536; //1344;
 const SAFE_ZONE_WIDTH=1200;
 const SAFE_ZONE_HEIGHT=900;
 
+var aspectRatioSafeZone = SAFE_ZONE_WIDTH / SAFE_ZONE_HEIGHT;
+var extraWidth = 0, extraHeight = 0;
+if (aspectRatioSafeZone < aspectRatioDevice) {
+	// have to add game pixels vertically in order to fill the device screen
+	extraWidth = aspectRatioDevice * SAFE_ZONE_HEIGHT - SAFE_ZONE_WIDTH;
+} else {
+	// have to add game pixels horizontally
+	extraHeight = SAFE_ZONE_WIDTH / aspectRatioDevice - SAFE_ZONE_HEIGHT;
+}
+
 //var game = new Phaser.Game( (h > w) ? h : w, (h > w) ? w : h, Phaser.CANVAS, 'game_div');
-var game = new Phaser.Game( SAFE_ZONE_WIDTH, SAFE_ZONE_HEIGHT, Phaser.AUTO, 'game_div');
+//var game = new Phaser.Game( SAFE_ZONE_WIDTH, SAFE_ZONE_HEIGHT, Phaser.AUTO, 'game_div');
+var game = new Phaser.Game( SAFE_ZONE_WIDTH + extraWidth, SAFE_ZONE_HEIGHT + extraHeight, Phaser.CANVAS, 'game_div');
 
 var game_state = {};
 
@@ -614,9 +634,9 @@ game_state.main.prototype = {
 
 		//game.debug.renderSpriteInfo(shooter1,10,600);
 		
-    // game.debug.renderText("w="+w+", h="+h+", window.devicePixelRatio="+window.devicePixelRatio,10,10);
-    // game.debug.renderText("game.width="+game.width+", game.height="+game.height,10,50);
-    // game.debug.renderText("screen.width="+screen.width+", screen.height="+screen.height,10,100);
+    game.debug.renderText("w="+w+", h="+h+", window.devicePixelRatio="+window.devicePixelRatio,10,10);
+    game.debug.renderText("game.width="+game.width+", game.height="+game.height,10,50);
+    game.debug.renderText("screen.width="+screen.width+", screen.height="+screen.height,10,100);
 
 	}
 
